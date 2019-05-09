864ed9915af5e9707df88ff311364c579948bb96
RitikaKaira <ritz3107@gmail.com>
2019-05-09 15:19:45 +0530

    1 file modified, 8 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AtthemoneyComponent } from './atthemoney/atthemoney.component';
+import { DefaultComponent } from './default/default.component';
+import { ImpliedvolatilityComponent } from './impliedvolatility/impliedvolatility.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AtthemoneyComponent ],
+  declarations: [ AppComponent, HelloComponent, AtthemoneyComponent, DefaultComponent, ImpliedvolatilityComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
