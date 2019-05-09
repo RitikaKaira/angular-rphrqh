0e4a3ccc26a0157da11535437c3608c5851be940
RitikaKaira <ritz3107@gmail.com>
2019-05-09 15:47:00 +0530

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { AtthemoneyComponent } from './atthemoney/atthemoney.component';
-import { DefaultComponent } from './default/default.component';
-import { ImpliedvolatilityComponent } from './impliedvolatility/impliedvolatility.component';
-import { OpeninterestComponent } from './openinterest/openinterest.component';
 import { PremiumdecayComponent } from './premiumdecay/premiumdecay.component';
+import { StockService } from './stock.service';
+import { OpeninterestComponent } from './openinterest/openinterest.component';
+import { ImpliedvolatalityComponent } from './impliedvolatality/impliedvolatality.component';
 import { PutcallratioComponent } from './putcallratio/putcallratio.component';
-import { SupportresistanceComponent } from './supportresistance/supportresistance.component';
 import { VixComponent } from './vix/vix.component';
+import { AtthemoneyComponent } from './atthemoney/atthemoney.component';
+import { SupportresistanceComponent } from './supportresistance/supportresistance.component';
+import { DefaultComponent } from './default/default.component';
+
+import { AppRoutingModule } from './approuting.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AtthemoneyComponent, DefaultComponent, ImpliedvolatilityComponent, OpeninterestComponent, PremiumdecayComponent, PutcallratioComponent, SupportresistanceComponent, VixComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule, ChartsModule, HttpModule,AppRoutingModule],
+  declarations: [ AppComponent, HelloComponent, PremiumdecayComponent, OpeninterestComponent, ImpliedvolatalityComponent, PutcallratioComponent, VixComponent, AtthemoneyComponent, SupportresistanceComponent, DefaultComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [StockService]
 })
 export class AppModule { }
